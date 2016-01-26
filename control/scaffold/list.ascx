1cacf1dd827e608bda3ec3142c4b52c261df5817
Masamoto Miyata <miyata@sincere-co.com>
2016-01-26 17:18:09 +0900

    Scaffoldのインスタンスを参照するところまで実装。

---
+++ control/scaffold/list.ascx
@@ -0,0 +1,4 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="list.ascx.cs" Inherits="_sdx_Control_Scaffold_list" %>
+
+
+<div>list</div>
---
+++ control/scaffold/list.ascx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _sdx_Control_Scaffold_list : System.Web.UI.UserControl
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      var scaffold = Sdx.Web.Scaffold.Instance(this.Name);
+      Sdx.Context.Current.Debug.Log(scaffold);
+    }
+
+    public string Name { get; set; }
+}
