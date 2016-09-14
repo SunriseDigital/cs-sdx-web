ca376c69614d1b2e8141916ab0060626191e0d2b
Masamoto Miyata <miyata@sincere-co.com>
2016-09-14 17:22:43 +0900

    Textにselectの機能を付与しました。

--- _private/js/modules/react/components/form/Text.js
+++ _private/js/modules/react/components/form/Text.js
@@ -2,14 +2,30 @@ import TextBase from './TextBase';
 
 export default class Text extends TextBase {
   createFormTag(target){
-    return (
-      <input
-        data-id={target.id}
-        className="form-control text-element"
-        type="text"
-        onChange={(e) => this.onValueChange(e)} value={target.value || ""}
-      />
-    )
+    if(this.props.data.select){
+      return (
+        <select
+          data-id={target.id}
+          className="form-control text-element"
+          onChange={(e) => this.onValueChange(e)} value={target.value || ""}
+        >
+          {this.props.data.select.map((option, key) => {
+            return (
+              <option key={key} value={option.value}>{option.label}</option>
+            )
+          })}
+        </select>
+      )
+    } else {
+      return (
+        <input
+          data-id={target.id}
+          className="form-control text-element"
+          type="text"
+          onChange={(e) => this.onValueChange(e)} value={target.value || ""}
+        />
+      )
+    }
   }
 }
 
