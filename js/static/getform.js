0809d6715f53debede099eb8026e4c688db8b815
Masamoto Miyata <miyata@sincere-co.com>
2016-12-06 14:33:13 +0900

    エンターを押したとき、外側のフォームがサブミットされてしまう問題に対応。

--- js/static/getform.js
+++ js/static/getform.js
@@ -1,14 +1,30 @@
 $(function () {
-  $('.sdx-getform .sdx-getform-submit').on("click", function (e) {
-    e.preventDefault();
-    var wrapper = $(this).closest(".sdx-getform");
+  function submit(wrapper){
     var action = wrapper.attr('data-action');
     if(!action) action = location.pathname;
     action += action.indexOf("?") === -1 ? '?' : '&';
-    location.href = action + $(this).closest(".sdx-getform").find("input, textarea, select").serialize();
+    location.href = action + wrapper.find("input, textarea, select").serialize();
+  }
+
+  //エンターキーで外側のフォームがサブミットされるのと止める。
+  $('.sdx-getform').find("input, select").on('keypress', function(e){
+    if(e.which == 13) {
+        e.preventDefault();
+        var wrapper = $(this).closest(".sdx-getform");
+        submit(wrapper);
+        return false;
+    }
+  });
+
+  //サブミットボタンのクリック
+  $('.sdx-getform .sdx-getform-submit').on("click", function (e) {
+    e.preventDefault();
+    var wrapper = $(this).closest(".sdx-getform");
+    submit(wrapper);
     return false;
   });
 
+  //全てのフォームのリセット
   $('.sdx-getform .sdx-getform-reset').on('click', function (e) {
     e.preventDefault();
     var $btn = $(this);
