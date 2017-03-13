eef55d95175e04d2deb9b4ffdcb819961c7c53c8
Masamoto Miyata <miyata@sincere-co.com>
2017-03-13 16:37:39 +0900

    ajaxフォームを追加。

---
+++ js/static/ajax-form.js
@@ -0,0 +1,18 @@
+(function(){
+  $.fn.sdxAjaxForm = function(options){
+    return this.each(function(){
+      var $wrapper = $(this);
+      var $submit = $wrapper.find('.sdx-ajax-form-submit');
+      var $elements = $wrapper.find('.sdx-ajax-form-elem');
+      var ajaxUrl =  $wrapper.attr("data-sdx-ajax-form-url");
+      $submit.on('click', function(e){
+        e.preventDefault();
+        options.jqXHR($.ajax({
+          url: ajaxUrl,
+          data: $elements.serializeArray()
+        }));
+        return false;
+      });
+    });
+  }
+})()
