7e7bed87822b50af25432e195f7b9d07ed891182
shimizu-k-digi <shimizu-k@sunrisedigital.jp>
2017-04-07 09:49:29 +0900

    カラーピッカー用のコンポーネント作成

---
+++ _private/js/modules/react/components/ColorPicker.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export default class ColorPicker extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount(){
+    console.log(this.props.color)
+    $(".color-picker").find("#" + this.props.id).spectrum({
+      color: this.props.color,
+    });
+  }
+
+  componentWillUpdate() {
+    $(".color-picker").find("#" + this.props.id).spectrum({
+      color : this.props.color
+    });
+  }
+
+  render() {    
+    return (
+      <div className="color-picker">
+        <input type="text" id={this.props.id} />
+      </div>
+    );
+  }
+}
