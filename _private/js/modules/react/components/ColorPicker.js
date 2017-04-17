7693080765aa94b797b4f12c88a741c6732875fa
shimizu-k-digi <shimizu-k@sunrisedigital.jp>
2017-04-17 17:06:19 +0900

    単純にinputのvalueにsetするように変更

--- _private/js/modules/react/components/ColorPicker.js
+++ _private/js/modules/react/components/ColorPicker.js
@@ -5,31 +5,20 @@ export default class ColorPicker extends Component {
     super(props);
   }
 
-  initializeColorPicker(props)
-  {
-    $(".color-picker").find("#" + props.id).spectrum({
-      color: props.color,
-      change: function(color){ 
-        props.onChangeColor(color.toHexString())
+  componentDidMount() {
+    const self = this;
+    $(this.refs.colorPicker).spectrum({
+      color: self.props.color,
+      change: function (color) {
+        self.props.onChangeColor(color.toHexString())
       }
     });
-  }
-
-  componentDidMount(){
-    this.initializeColorPicker(this.props)
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if(this.props.color != nextProps.color)
-    {
-      this.initializeColorPicker(nextProps);
-    }
   }
 
   render() {
     return (
       <div className="color-picker">
-        <input type="text" id={this.props.id} />
+        <input type="text" value={this.props.color} ref="colorPicker" />
       </div>
     );
   }
